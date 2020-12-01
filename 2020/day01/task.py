6c3bbecfc6217327131362a096145da607ec6041
YAWNICK <jaweiss1999@googlemail.com>
2020-12-01 06:27:46 +0100

    Day 1

--- 2020/day01/task.py
+++ 2020/day01/task.py
@@ -1,22 +1,47 @@
-
+import os
 
 def compute(s: str):
     ## if single value:
     #s = s.strip()
     ## if multiple values in multiple lines
-    ## assuming no newline at the end of the input file
-    #lines = s.splitlines()
-    
-    return 
+    lines = s.splitlines()
+    lines = lines[:-1] if lines[-1] == '' else lines
+    num1 = 0
+    num2 = 0
+    num3 = 0
+    for l in lines:
+        for s in lines:
+            for t in lines:
+                if int(l) + int(s) + int(t) == 2020:
+                    num1 = int(l)
+                    num2 = int(s)
+                    num3 = int(t)
+    return num1 * num2 * num3
 
-def read_input() -> str:
+def read_input(filepath: str) -> str:
     inp = ""
-    with open("input.txt", 'r') as f:
+    with open(filepath, 'r') as f:
         inp = f.read()
     return inp
 
+def run_tests():
+    print("-----TESTS-----")
+    # Fill solutions with should values
+    solutions = []
+    # testing begins here
+    cnt = 1
+    while os.path.exists(f"tests/{cnt}.txt"):
+        if compute(read_input(f"tests/{cnt}.txt")) == solutions[cnt - 1]:
+            print(f"Test {cnt} successful!")
+        else:
+            print(f"Test {cnt} failed!")
+            print(f"  was supposed to be {solutions[cnt - 1]}")
+        cnt += 1
+    print("---TESTS END---")
+
 def main():
-    inp = read_input()
+    run_tests()
+    inp = read_input("input.txt")
     print(compute(inp))
 
 if __name__ == "__main__":
