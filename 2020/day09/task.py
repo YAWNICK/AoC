7d14a7e45ae113c0576c022b601dc137d7ea5c4a
YAWNICK <jaweiss1999@googlemail.com>
2020-12-09 06:36:28 +0100

    day 9

--- 2020/day09/task.py
+++ 2020/day09/task.py
@@ -1,25 +1,25 @@
 import os
 
+def somesum(n, l):
+    for i in l:
+        for j in l:
+            if i != j and i + j == n:
+                return True
+    return False
+
 def compute(s: str):
     ## if single value:
     #s = s.strip()
     ## if multiple values in multiple lines
     lines = s.splitlines()
     lines = lines[:-1] if lines[-1] == '' else lines
-    accu = 0
-    ip = 0
-    visited = set()
-    while ip not in visited:
-        ins, n = lines[ip].split(' ')
-        visited.add(ip)
-        if ins == 'acc':
-            accu += int(n)
-            ip += 1
-        elif ins == 'jmp':
-            ip += int(n)
-        else:
-            ip += 1
-    return accu
+    lines = list(map(int, lines))
+    for i in range(25, len(lines)):
+        n = lines[i]
+        nums = lines[i-25:i]
+        if not somesum(n, nums):
+            return n
+    return None
 
 def compute2(s: str):
     ## if single value:
@@ -27,25 +27,16 @@ def compute2(s: str):
     ## if multiple values in multiple lines
     lines = s.splitlines()
     lines = lines[:-1] if lines[-1] == '' else lines
+    lines = list(map(int, lines))
+    n = 88311122
     for i in range(len(lines)):
-        accu = 0
-        ip = 0
-        visited = set()
-        while ip not in visited and ip != 683:
-            ins, n = lines[ip].split(' ')
-            visited.add(ip)
-            if ip == i and ins in ['nop', 'jmp']:
-                ins = ['nop', 'jmp'][ins == 'nop']
-
-            if ins == 'acc':
-                accu += int(n)
-                ip += 1
-            elif ins == 'jmp':
-                ip += int(n)
-            else:
-                ip += 1
-        if ip == 683:
-            return accu
+        nums = [lines[i]]
+        j = 1
+        while sum(nums) < n:
+            nums.append(lines[i+j])
+            j += 1
+        if sum(nums) == n:
+            return min(nums) + max(nums)
 
 def read_input(filepath: str) -> str:
     inp = ""
@@ -71,8 +62,8 @@ def run_tests():
 def main():
     run_tests()
     inp = read_input("input.txt")
-    print("Part 1", compute(inp))
-    print("Part 2", compute2(inp))
+    print("Part 1:", compute(inp))
+    print("Part 2:", compute2(inp))
 
 if __name__ == "__main__":
     main()
